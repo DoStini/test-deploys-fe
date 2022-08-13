80c45ceae7593fe10f1b518f6ea4545c33d05d88
André Moreira <andre.moreira.9@hotmail.com>
2022-08-13 14:55:17 +0200

    Add simple request

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React, {useEffect, useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const [data, setData] = useState(undefined);
+    const url = process.env.BASE_URL || "http://localhost:7070";
+
+    const log = console.log;
+    log(url + "/data")
+    useEffect(() => {
+        fetch(url + "/data")
+            .then(data => (log("success:"), data.json().then(data => setData(data.data))))
+            .catch(_ => (log("error:", _), setData("error")))
+    }, [url]);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {data ?
+            <p>
+                Data: {data} 
+            </p>
+            : <p>Loading info</p>
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
